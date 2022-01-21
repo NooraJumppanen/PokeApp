90efff664db27300245f12fefd5c7af0fd0c0d51
NooraJumppanen <noora.jumppanen@gmail.com>
2022-01-21 21:46:55 +0200

    first init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Spinner from 'react-bootstrap/Spinner';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [pokemons, setPokemons] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		axios.get('https://pokeapi.co/api/v2/pokemon/').then((response) => {
+			const fetches = response.data.results.map((p) =>
+				axios.get(p.url).then((response) => response.data)
+			);
+			Promise.all(fetches).then((data) => {
+				setPokemons(data);
+				setIsLoading(false);
+			});
+		});
+	}, []);
+	// console.log('state after GET', pokemons);
+
+	return (
+		<div>
+			<Navbar bg="dark" variant="dark">
+				<Container>
+					<Navbar.Brand href="#">PokeApp</Navbar.Brand>
+				</Container>
+			</Navbar>
+
+			<Container>
+				<Row
+					xs={2}
+					md={4}
+					lg={5}
+					className="justify-content-between my-5 d-flex gap-3"
+				>
+					{isLoading && (
+						<Spinner animation="border" role="status">
+							<span className="visually-hidden">Loading...</span>
+						</Spinner>
+					)}
+
+					{!isLoading &&
+						pokemons.map((pokemon) => (
+							<Card
+								bg="dark"
+								text="light"
+								key={pokemon.name}
+								style={{ width: '14rem' }}
+							>
+								<Card.Header>{pokemon.name}</Card.Header>
+								<Card.Body>
+									<Card.Img
+										variant="top"
+										src={pokemon.sprites.other.dream_world.front_default}
+										className="poke_image"
+									/>
+								</Card.Body>
+							</Card>
+						))}
+				</Row>
+			</Container>
+		</div>
+	);
+};
 
 export default App;
